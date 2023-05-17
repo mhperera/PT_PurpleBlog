71f03536098b80c93bbbc20c63962eb999c5cc12
zmhp <manashahp1995@gmail.com>
2023-05-17 23:41:40 +0530

    Removed unused hooks

--- src/components/EditPostComponent.js
+++ src/components/EditPostComponent.js
@@ -1,9 +1,8 @@
-import React, { useContext } from 'react';
-import { useState, useEffect } from 'react';
+import React from 'react';
+import {  useEffect } from 'react';
 import { format } from 'date-fns';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { useStoreState, useStoreActions } from 'easy-peasy';
-import api from './../api/posts';
 
 const EditPostComponent = () => {
 	// const { posts, format, api, navigate, setPosts } = useContext(DataContext);
